c9d02fac19be8ff5defb57b1cbfea33260a2de56
Ricky Roberts <Richard.robertsf@gmail.com>
2024-10-31 12:45:40 -0400

    code for task 5

--- ROBERRF_results.py
+++ ROBERRF_results.py
@@ -60,3 +60,35 @@ MISSING = ["99.99", "999.9", "9999.9"]
 #           minT[2] = row["DATE"]
 #           minT[3] = row["MIN"]
 # print('OVERALL MIN TEMP: {}, {}, {}, {}'.format(minT[3], minT[2], minT[1], minT[0]))
+
+# 5. Find the year with the most precipitation for Cincinnati and Florida
+cin, fla = ['', '', '', 0.0], ['', '', '', 0.0]
+for year in range(2015, 2025):
+  for dataset in os.listdir(str(year)):
+    with open(str(year) + '/' + dataset, 'r') as file:
+      reader = csv.DictReader(file)
+      next(reader)
+      prcp_sum, row_count = 0.0, 0.0
+      curr_station, curr_name = '', ''
+      for i, row in enumerate(reader):
+        if (i == 0):
+          curr_station = row["STATION"]
+          curr_name = row["NAME"]
+        prcp_sum += float(row["PRCP"]) 
+        row_count += 1.0
+      prcp_mean = prcp_sum / row_count
+      
+      if (dataset[:-4] == CINCINNATI):
+        if (prcp_mean > cin[3]):
+          cin[0] = curr_station
+          cin[1] = curr_name
+          cin[2] = year
+          cin[3] = prcp_mean
+      else:
+        if (prcp_mean > fla[3]):
+          fla[0] = curr_station
+          fla[1] = curr_name
+          fla[2] = year
+          fla[3] = prcp_mean
+print('Highest PRCP mean Cincinnati: {}, {}, {}, {}'.format(cin[3], cin[2], cin[1], cin[0]))
+print('Highest PRCP mean Florida: {}, {}, {}, {}'.format(fla[3], fla[2], fla[1], fla[0]))
